d8945f4f621f00a6886f9a89cf4b6e3f33100245
swnordstrom <swnordstrom@email.wm.edu>
2020-06-15 12:36:06 -0600

    init a script for generating bottlenecked populations

---
+++ simulations/prepare_simulations/generate_bottlenecked_popns.R
@@ -0,0 +1,128 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(tidyselect)
+
+rm(list = ls())
+
+source('base_model_source/sim_functions.R')
+
+##### Parameters
+
+n.trials = 500
+n.loci = 25
+theta = 0
+wfitn = sqrt(1 / 0.14 / 2)
+
+##### Run simulations
+
+liszt = vector('list', n.trials)
+
+set.seed(87795)
+
+for (trial in 1:n.trials) {
+  liszt[[trial]] = sim(
+    a = c(-1/2, 1/2),
+    params = data.frame(end.time = 8,
+                        init.row = 1e2,
+                        n.loci = n.loci, 
+                        n.pop0 = 2,
+                        w.max = 2, 
+                        theta = theta, 
+                        wfitn = wfitn,
+                        sig.e = 0)
+  )
+  print(trial)
+}
+
+all.trials = unroller(liszt)
+
+head(all.trials)
+
+init.pops = all.trials %>%
+  group_by(trial) %>%
+  filter(mean(fem[gen %in% 1]) == 0.5)
+
+length(unique(init.pops$trial))
+
+pop20s = init.pops %>%
+  group_by(trial, gen) %>%
+  mutate(n = n()) %>%
+  group_by(trial) %>%
+  filter(any(n > 20)) %>%
+  ungroup() %>%
+  select(-n)
+
+pop100s = init.pops %>%
+  group_by(trial, gen) %>%
+  mutate(n = n()) %>%
+  group_by(trial) %>%
+  filter(any(n > 100)) %>%
+  ungroup() %>%
+  select(-n)
+
+nrow(pop20s)
+length(unique(pop20s$trial))
+nrow(pop100s)
+length(unique(pop100s$trial))
+
+pop100s %>%
+  group_by(trial, gen) %>%
+  summarise(n = n()) %>%
+  group_by(trial) %>%
+  filter(n == max(n)) %>%
+  ungroup() %>%
+  select(n) %>% unlist() %>% hist()
+
+# One question that just occurred to me... does it matter when a population is selected? Should I sample from the first time step with >100 individuals, or all at the same timestep?
+
+var.t.20s = pop20s %>%
+  select(-c(g_i, z_i, w_i, r_i, fem)) %>%
+  gather(key = loc.copy, value = val, -c(trial, i, gen)) %>%
+  mutate(locus = gsub('^[ab]', '', loc.copy)) %>%
+  group_by(trial, gen, locus) %>%
+  summarise(p = mean(val > 0),
+            n = length(unique(i))) %>%
+  group_by(trial, gen) %>%
+  summarise(var.t = sum(2 * p * (1 - p) / 25),
+            n.t = n[1])
+
+var.t.20s %>%
+  group_by(gen) %>%
+  mutate(vt.bar = mean(var.t)) %>%
+  ggplot() +
+  geom_segment(aes(x = 1, xend = 8, y = 0.48, yend = 0.48)) +
+  geom_line(aes(x = gen, y = var.t, group = trial)) +
+  geom_point(aes(x = gen, y = var.t, fill = log(n.t / 20)),
+             shape = 21, colour = 'black', stroke = 1) +
+  geom_line(aes(x = gen, y = vt.bar), linetype = 3,
+            colour = 'red', size = 3) +
+  scale_fill_gradient2(low = 'darkblue', mid = 'white', high = 'red') +
+  theme(panel.background = element_rect('white'))
+
+# Do the same but for the populations of size 100
+
+var.t.100s = pop100s %>%
+  select(-c(g_i, z_i, w_i, r_i, fem)) %>%
+  gather(key = loc.copy, value = val, -c(trial, i, gen)) %>%
+  mutate(locus = gsub('^[ab]', '', loc.copy)) %>%
+  group_by(trial, gen, locus) %>%
+  summarise(p = mean(val > 0),
+            n = length(unique(i))) %>%
+  group_by(trial, gen) %>%
+  summarise(var.t = sum(2 * p * (1 - p) / 25),
+            n.t = n[1])
+
+var.t.100s %>%
+  group_by(gen) %>%
+  mutate(vt.bar = mean(var.t)) %>%
+  ggplot() +
+  geom_segment(aes(x = 1, xend = 8, y = 0.48, yend = 0.48)) +
+  geom_line(aes(x = gen, y = var.t, group = trial)) +
+  geom_point(aes(x = gen, y = var.t, fill = log(n.t / 100)),
+             shape = 21, colour = 'black', stroke = 1) +
+  geom_line(aes(x = gen, y = vt.bar), linetype = 3,
+            colour = 'red', size = 3) +
+  scale_fill_gradient2(low = 'darkblue', mid = 'white', high = 'red') +
+  theme(panel.background = element_rect('white'))
+
