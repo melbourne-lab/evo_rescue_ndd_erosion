e66ac57d2029e18d2b8379ad4ec005ab1082d1fd
swnordstrom <swnordstrom@email.wm.edu>
2021-02-11 15:03:02 -0700

    prettify figs

--- experiment/expmt_bottleneck24/plot_results.R
+++ experiment/expmt_bottleneck24/plot_results.R
@@ -7,9 +7,12 @@ library(tidyr)
 expmt.sum = read.csv('experiment/expmt_bottleneck24/out/sim_results.csv')
 
 # Plot with treatments in panels
-experiment.summ %>%
+expmt.sum %>%
   group_by(trial) %>%
   mutate(extinct = max(gen) < 15) %>%
+  ungroup() %>%
+  mutate(n.pop0     = factor(n.pop0 > 20, labels = c('small', 'large')),
+         alpha      = factor(alpha  > 0,  labels = c('dens indep', 'dens dep'))) %>%
   ggplot(aes(x = gen, y = n)) +
   geom_line(
     aes(
@@ -23,10 +26,11 @@ experiment.summ %>%
   scale_linetype_manual(values = 2:1) +
   scale_size_manual(values = c(0.25, 0.75)) +
   scale_y_log10() +
-  facet_wrap(n.pop0 ~ alpha)
+  facet_wrap(~ paste(n.pop0, alpha, sep = ', ')) +
+  theme(legend.position = 'bottom')
 
 # Plot with source population in panel
-experiment.summ %>%
+expmt.sum %>%
   ggplot(aes(x = gen, y = n)) +
   geom_line(
     aes(
@@ -47,4 +51,6 @@ experiment.summ %>%
   scale_shape_manual(values = c(1, 21)) +
   scale_y_log10() +
   facet_wrap(~ sourcepops, nrow = 4) +
-  theme(legend.position = 'none')
+  theme(legend.position = 'none',
+        panel.grid      = element_blank(),
+        panel.spacing   = unit(0.1, 'lines'))
