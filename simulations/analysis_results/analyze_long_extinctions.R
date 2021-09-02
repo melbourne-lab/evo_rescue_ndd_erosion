4fe22b06ce16d5d47b7ebba1916e1743669b21ce
swnordstrom <swnordstrom@email.wm.edu>
2021-09-02 10:46:00 -0600

    needs prettifying - but - add in more model stuff, including Bayesian models and figure with Bayesian results

--- simulations/analysis_results/analyze_long_extinctions.R
+++ simulations/analysis_results/analyze_long_extinctions.R
@@ -45,7 +45,10 @@ all.extinctions.long = all.data %>%
   summarise(n.extinctions = sum(ext.this.gen)) %>%
   ungroup() %>%
   arrange(n.pop0, low.var, alpha, gen) %>%
-  mutate(n.pop0 = factor(ifelse(n.pop0 %in% 100, "Large", "Small")),
+  mutate(romnum = factor(ifelse(n.pop0 %in% 100,
+                                ifelse(low.var, '(i)', '(ii)'),
+                                ifelse(low.var, '(iii)', '(iv)'))),
+         n.pop0 = factor(ifelse(n.pop0 %in% 100, "Large", "Small")),
          alpha = factor(ifelse(alpha > 0, "Density dependent", "Density independent")),
          low.var = factor(ifelse(low.var, "Low diversity", "High diversity")))
 
@@ -59,7 +62,14 @@ ext.mod.final =  glm(
   family = 'binomial'
 )
 
+ext.mod.full =  glm(
+  extinct ~ factor(n.pop0) * low.var * factor(alpha) * gbar,
+  data = ext.data, 
+  family = 'binomial'
+)
+
 summary(ext.mod.final)
+summary(ext.mod.full)
 
 ### Make plots
 
@@ -177,6 +187,47 @@ geno.plot = geno.ext.preds %>%
         panel.background = element_rect(fill = 'white'),
         strip.background = element_rect(colour = 'black'))
 
+geno.ext.fulls = expand.grid(gbar = (-5:5)/10,
+                             alpha = c(0, 0.0035),
+                             n.pop0 = factor(c(20, 100)),
+                             low.var = c(TRUE, FALSE)) %>%
+  (function(x) cbind(x, p = predict(ext.mod.full, x, type = 'response'))) %>%
+  mutate(n.pop0 = factor(ifelse(n.pop0 %in% 100, "Large", "Small")),
+         alpha = factor(ifelse(alpha > 0, "Density dependent", "Density independent")),
+         low.var = factor(ifelse(low.var, "Low diversity", "High diversity")))
+
+geno.full = geno.ext.fulls %>%
+  ggplot() +
+  geom_line(
+    aes(
+      x = gbar, y = p, group = factor(alpha),
+      colour = factor(alpha)
+    )
+  ) +
+  # geom_point(
+  #   data = all.data %>% 
+  #     distinct(trial, n.pop0, low.var, alpha, .keep_all = TRUE) %>%
+  #     mutate(ext.jitter = as.numeric(extinct) + (1 - 2*as.numeric(extinct)) * runif(nrow(.), 0, 0.1)),
+  #   aes(
+  #     x = gbar, y = ext.jitter, colour = factor(alpha)
+  #   ),
+  #   alpha = 0.1
+  # ) +
+  # geom_text(
+  #   data = pan.labls,
+#   aes(
+#     x = 0, y = 0.7,
+#     label = labl
+#   )
+# ) +
+  labs(x = 'Initial genotype', y = '') +
+  scale_color_manual(values = c('purple', 'black')) +
+  facet_wrap( ~ paste(n.pop0, low.var, sep = ', '), ncol = 1) +
+  theme(legend.position = 'none',
+        panel.grid.major = element_line(colour = 'gray88'),
+        panel.background = element_rect(fill = 'white'),
+        strip.background = element_rect(colour = 'black'))
+
 extinct.legend = get_legend(
   inst.plot + 
     guides(fill = guide_legend(''), colour = guide_legend('')) +
@@ -184,7 +235,7 @@ extinct.legend = get_legend(
           legend.box.margin = margin(1, 0, 0, 0))
 )
 
-data.plots = plot_grid(cuml.plot, inst.plot, geno.plot, 
+data.plots = plot_grid(cuml.plot, inst.plot, geno.full, 
                        labels = c('(A)', '(B)', '(C)'),
                        nrow = 1)
 
@@ -205,7 +256,10 @@ ext.data %>%
 
 size.plot.row = all.data %>%
   filter(trial < 26) %>%
-  mutate(n.pop0 = factor(ifelse(n.pop0 %in% 100, "Large", "Small")),
+  mutate(romnum = factor(ifelse(n.pop0 %in% 100,
+                                ifelse(low.var, '(i)', '(ii)'),
+                                ifelse(low.var, '(iii)', '(iv)'))),
+         n.pop0 = factor(ifelse(n.pop0 %in% 100, "Large", "Small")),
          alpha = factor(ifelse(alpha > 0, "Density dependent", "Density independent")),
          low.var = factor(ifelse(low.var, "Low diversity", "High diversity"))) %>%
   ggplot(aes(x = gen)) +
@@ -233,7 +287,7 @@ size.plot.row = all.data %>%
   scale_alpha_manual(values = c(0, 1)) +
   scale_fill_manual(values = c('purple', 'black')) +
   scale_color_manual(values = c('purple', 'black')) +
-  facet_wrap( ~ paste(n.pop0, low.var, sep = ', '), ncol = 4) +
+  facet_wrap( ~ paste0(romnum, ' ', n.pop0, ', ', low.var), ncol = 4) +
   theme(legend.position = 'none',
         panel.grid.major = element_line(colour = 'gray88'),
         panel.background = element_rect(fill = 'white'),
@@ -293,7 +347,7 @@ cuml.plot.row = all.extinctions.long %>%
         panel.grid.major = element_line(colour = 'gray88'),
         panel.background = element_rect(fill = 'white'))
 
-geno.plot.row = geno.ext.preds %>%
+geno.full.row = geno.ext.fulls %>%
   ggplot() +
   geom_line(
     aes(
@@ -311,10 +365,124 @@ geno.plot.row = geno.ext.preds %>%
         panel.background = element_rect(fill = 'white'))
 
 data.plots.row = plot_grid(size.plot.row, cuml.plot.row, 
-                           inst.plot.row, geno.plot.row, 
+                           inst.plot.row, geno.full.row, 
                            labels = c('(A)', '(B)', '(C)', '(D)'),
                            nrow = 4)
 
 plot_grid(data.plots.row, extinct.legend, ncol = 1, rel_heights = c(1, .1)) %>%
   save_plot(filename = 'simulations/analysis_results/figure_drafts/draft_figs/fig_2_long_rows.pdf',
             base_width = 8, base_height = 8)
+
+##### Bayesian model
+
+library(rstanarm)
+
+ext.data = ext.data %>%
+  mutate(low.var = factor(ifelse(low.var, 'low.div', 'hi.div')),
+         n.pop0  = factor(ifelse(n.pop0 > 50, 'large', 'small')),
+         alpha   = factor(ifelse(alpha > 0, 'ndd', 'di')))
+
+ext.mod.full =  stan_glm(
+  extinct ~ n.pop0 * low.var * alpha * gbar,
+  data = ext.data, 
+  seed = 992,
+  cores = 4,
+  prior = normal(0, 5),
+  family = 'binomial'
+)
+
+summary(ext.mod.full)
+
+bayesplot::pp_check(ext.mod.full)
+
+plot(ext.mod.full$fitted.values)
+
+posts = as.data.frame(ext.mod.full)
+
+# NDD in large, high diversity
+mean(posts$alphandd)
+
+# NDD in large, low diversity
+mean(posts$alphandd + posts$`low.varlow.div:alphandd`)
+
+# NDD in small, high diversity populations
+mean(posts$alphandd + posts$`n.pop0small:alphandd`)
+
+ndd.posts = with(
+  posts,
+  cbind(
+    large.hivar = alphandd,
+    large.lovar = alphandd + `low.varlow.div:alphandd`,
+    small.hivar = alphandd + `n.pop0small:alphandd`,
+    small.lovar = alphandd + `low.varlow.div:alphandd` + `n.pop0small:alphandd` + `n.pop0small:low.varlow.div:alphandd`
+    )
+  )
+
+apply(ndd.posts, 2, mean) %>% round(2)
+apply(ndd.posts, 2, function(x) quantile(x, c(0.025, 0.975))) %>% round(2)
+
+size.posts = with(
+  posts,
+  cbind(
+    hivar.dindp = n.pop0small,
+    hivar.ddepn = n.pop0small + `n.pop0small:alphandd`,
+    lovar.dindp = n.pop0small + `n.pop0small:low.varlow.div`,
+    lovar.ddepn = n.pop0small + `n.pop0small:alphandd` + `n.pop0small:low.varlow.div` + `n.pop0small:low.varlow.div:alphandd`
+  )
+)
+
+apply(size.posts, 2, mean) %>% round(2)
+apply(size.posts, 2, function(x) quantile(x, c(0.025, 0.975))) %>% round(2)
+
+gdiv.posts = with(
+  posts,
+  cbind(
+    large.dindp = low.varlow.div,
+    large.ddepn = low.varlow.div + `low.varlow.div:alphandd`,
+    small.dindp = low.varlow.div + `n.pop0small:low.varlow.div`,
+    small.ddepn = low.varlow.div + `low.varlow.div:alphandd` + `n.pop0small:low.varlow.div` + `n.pop0small:low.varlow.div:alphandd`
+  )
+)
+
+apply(gdiv.posts, 2, mean) %>% round(2)
+apply(gdiv.posts, 2, function(x) quantile(x, c(0.025, 0.975))) %>% round(2)
+
+### Get estimates for 
+
+geno.ext.fulls = expand.grid(gbar = (-5:5)/10,
+                             alpha = c("di", "ndd"),
+                             n.pop0 = c("large", "small"),
+                             low.var = c("low.div", "hi.div"))
+
+epreds = posterior_epred(ext.mod.full, newdata = geno.ext.fulls,
+                         seed = 196200, draws = 200) %>%
+  as.data.frame() %>%
+  t() %>%
+  cbind(geno.ext.fulls, .)
+
+epreds = epreds %>%
+  gather(key = draw, value = estimate, -c(gbar, alpha, n.pop0, low.var))
+
+geno.full.row = ggplot(epreds, aes(x = gbar, y = estimate)) +
+  geom_line(
+    aes(
+      group = interaction(alpha, draw),
+      colour = factor(alpha)
+    ),
+    size = 0.1
+  ) +
+  labs(x = 'Initial genotype', y = '') +
+  scale_color_manual(values = c('black', 'purple')) +
+  facet_wrap( ~ paste(n.pop0, low.var, sep = ', '), ncol = 4) +
+  theme(legend.position = 'none',
+        strip.text = element_blank(),
+        strip.background = element_blank(),
+        panel.grid.major = element_line(colour = 'gray88'),
+        panel.background = element_rect(fill = 'white'))
+
+data.plots.row = plot_grid(size.plot.row, inst.plot.row, 
+                           cuml.plot.row, geno.full.row, 
+                           labels = c('(A)', '(B)', '(C)', '(D)'),
+                           nrow = 4)
+
+data.plots.row
