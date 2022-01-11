4d9e199c5dae68fa01d2d853fee1b22c5e94437b
swnordstrom <swnordstrom@email.wm.edu>
2022-01-10 18:57:48 -0700

    try two-way model fitting

--- simulations/analysis_results/test_code/test_biostan_weibullmod.R
+++ simulations/analysis_results/test_code/test_biostan_weibullmod.R
@@ -68,6 +68,7 @@ stan.inputs0 = list(
 
 stan0.fit = stan(file = 'simulations/weibull_survival_model.stan',
                  data = stan.inputs0,
+                 seed = 10,
                  cores = 4)
 
 stan0.fit 
@@ -76,3 +77,69 @@ stan0.fit
 
 # taus: I don't think these are interesting...
 # what are we actually interested in? I guess alpha, mu, beta_bg?
+
+stan0.pars = as.data.frame(stan0.fit) %>%
+  rename(beta = `beta_bg[1]`) %>%
+  mutate(i = rep(1:4000)) %>%
+  gather(key = par, value = estim, -i) %>%
+  mutate(chain = (i-1) %/% 1000 + 1,
+         iter  = (i-1) %% 1000 + 1)
+  
+ggplot(stan0.pars %>% filter(par %in% c('mu', 'alpha', 'beta'))) +
+  geom_line(aes(x = iter, y = estim, group = chain),
+            size = 0.5) +
+  facet_wrap(chain ~ par, scales = 'free_y', nrow = 4)
+
+# Chains 1 and 3 look bad for a few iterations
+
+bayesplot::mcmc_areas(stan0.fit, pars = c('mu', 'alpha', 'beta_bg[1]'))
+
+### Now, can this code work with a two-way interaction?
+
+ext1 = ext.times %>% filter(n.pop0 < 50)
+
+mod.mat1 = ext1 %>%
+  ungroup() %>%
+  mutate(b_alpha = as.numeric(alpha > 0),
+         b_lovar = as.numeric(low.var),
+         b_alpha_lovar = b_alpha * b_lovar) %>%
+  select(b_alpha, b_lovar, b_alpha_lovar)
+
+stan.inputs1 = list(
+  Nobs = sum(ext1$extinct),
+  Ncen = sum(!ext1$extinct),
+  M_bg = 3,
+  yobs = ext1$ext.gen[ext1$extinct],
+  ycen = ext1$ext.gen[!ext1$extinct],
+  Xobs_bg = as.matrix(mod.mat1[ext1$extinct,]),
+  Xcen_bg = as.matrix(mod.mat1[!ext1$extinct,])
+)
+
+stan1.fit = stan(file = 'simulations/weibull_survival_model.stan',
+                 data = stan.inputs1,
+                 seed = 10,
+                 cores = 4)
+
+# The code works
+
+stan1.fit
+# what to conclude when ESS for a raw parameter is low but is high for transformed??
+
+stan1.pars = as.data.frame(stan1.fit) %>%
+  rename(beta1 = `beta_bg[1]`,
+         beta2 = `beta_bg[2]`,
+         beta3 = `beta_bg[3]`) %>%
+  mutate(i = rep(1:4000)) %>%
+  gather(key = par, value = estim, -i) %>%
+  mutate(chain = (i-1) %/% 1000 + 1,
+         iter  = (i-1) %% 1000 + 1)
+
+ggplot(stan1.pars %>% filter(par %in% c('mu', 'alpha', 'beta1', 'beta2', 'beta3'))) +
+  geom_line(aes(x = iter, y = estim, group = chain),
+            size = 0.5) +
+  facet_wrap(chain ~ par, scales = 'free_y', nrow = 4)
+# wtf is happening in chain 4...?
+
+bayesplot::mcmc_areas(as.data.frame(stan1.fit)[1:2800,], 
+                      pars = c('mu', 'alpha', 'beta_bg[1]', 'beta_bg[2]', 'beta_bg[3]'))
+
